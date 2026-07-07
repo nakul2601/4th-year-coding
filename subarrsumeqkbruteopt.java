c85d88fc31e0c91e86ecc56fd6adec2578058f3d
nakul2601 <157724027+nakul2601@users.noreply.github.com>
2026-07-07 11:00:27 +0530

    optimized code of sum of sub array of k brute force

---
+++ subarrsumeqkbruteopt.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class subarrsumeqkbruteopt {
+
+    public static int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1); // Prefix sum 0 occurs once
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int num : nums) {
+            prefixSum += num;
+
+            if (map.containsKey(prefixSum - k)) {
+                count += map.get(prefixSum - k);
+            }
+
+            map.put(prefixSum, map.getOrDefault(prefixSum, 0) + 1);
+        }
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+
+        int k = sc.nextInt();
+
+        System.out.println(subarraySum(nums, k));
+
+        sc.close();
+    }
+}
